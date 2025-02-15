a37a428300c0a19c281f6375fed4080f14ff34de
souravsharma <msg2souravsharma@gmail.com>
2025-02-15 22:25:59 +1100

    initial commit

---
+++ game.js
@@ -0,0 +1,71 @@
+let array = ["red", "blue", "green", "yellow"]
+let hasStarted = false; 
+function nextSquence(){
+    let randomNumber = Math.floor(Math.random()*4)
+    return array[randomNumber]
+}
+function pattern(){
+
+    
+    let sequence = []
+    let value = nextSquence();
+    sequence.push(value)
+    let lastColor = sequence[sequence.length -1]
+    buttonAnimation(lastColor)
+    console.log("The "+ sequence + " The last color is: " + lastColor)
+    if (!hasStarted) { 
+        hasStarted = true; 
+        return;
+    }
+      
+    userTurn(sequence, lastColor)
+
+    
+}   
+function userTurn(sequence, lastColor){
+    let gameSelectedColor = lastColor 
+    function userTurn(sequence, lastColor){
+    let gameSelectedColor = lastColor 
+    $(document).on("click", (event) => {
+        console.log(event.target)
+
+    })
+    
+    // let value = userClick.hasClass(gameSelectedColor)
+    // console.log(value)
+    // if() {
+    //     addingSound("wrong")
+    // }
+    // $(gameSelectedColor).click(pattern)
+
+}
+    
+    // let value = userClick.hasClass(gameSelectedColor)
+    // console.log(value)
+    // if() {
+    //     addingSound("wrong")
+    // }
+    // $(gameSelectedColor).click(pattern)
+
+}
+$(document).on("keypress", pattern)
+
+function addingSound(color){
+    let soundURL = "./sounds/" + color + ".mp3"
+    let soundObj = new Audio(soundURL);
+    soundObj.play()
+
+    setTimeout(()=> {
+        soundObj.pause()
+
+    }, 1000)
+}
+
+function buttonAnimation(color){
+    $("." + color).addClass("pressed")
+    addingSound(color)
+    setTimeout( () =>  {
+        $("." + color).removeClass("pressed")
+    }, 10)
+   
+}
